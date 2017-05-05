ace53c17be13016af168d131208ca8a7f57a0c38
Joe Reeve <joseph.reeve@googlemail.com>
2017-05-05 05:20:36 +0100

    basic changes

---
+++ index.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+
+import * as EJS from 'ejs';
+import { markdown } from 'markdown';
+
+const catalog = require('./catalog.json');
+
+const templates = {
+    welcome: fs.readFileSync('./templates/welcome.ejs', 'utf8'),
+    page: fs.readFileSync('./templates/page.ejs', 'utf8'),
+    heuristic: fs.readFileSync('./templates/heuristic.ejs', 'utf8'),
+    term: fs.readFileSync('./templates/term.ejs', 'utf8')
+};
+
+catalog.heuristics.map(h => {
+    h.body = markdown.toHTML(fs.readFileSync(h.filename, 'utf8'));
+
+    const content = EJS.render(templates.heuristic, h);
+    const page = EJS.render(templates.page, { content });
+    fs.writeFileSync(`./dist/${h.title}.html`, page, 'utf8');
+});
+
+Object.keys(catalog.terms)
+    .forEach(key => {
+        const term = catalog.terms[key];
+        term.body = markdown.toHTML(fs.readFileSync(term.filename, 'utf8'));
+        const content = EJS.render(templates.term, term);
+        const page = EJS.render(templates.page, { content });
+        fs.writeFileSync(`./dist/term_${key}.html`, page, 'utf8');
+    });
+
+const welcomeBody = markdown.toHTML(fs.readFileSync('./terms/welcome.md', 'utf8'));
+const welcomeContent = EJS.render(templates.welcome, { content: welcomeBody, ...catalog });
+const welcomePage = EJS.render(templates.page, { content: welcomeContent });
+fs.writeFileSync(`./dist/index.html`, welcomePage, 'utf8');
---
+++ terms/location.md
@@ -0,0 +1,3 @@
+Location!
+
+Also known as position
